18986db1b3058ac74e620ccc9be779c96ca47877
chilimango <maldonadot55@yahoo.com>
2020-10-21 16:29:56 -0700

    server.js file made

---
+++ models/workout.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema
+const workoutSchema = new Schema({
+    day: {
+        type: Date,
+        default: new Date(),
+    },
+    exercises: [{
+            type: {
+                type: String,
+                required: true
+            },
+            name: {
+                type: String,
+                required: true
+            },
+            duration: {
+                type: Number,
+                required: true
+            },
+            weight: {
+                type: Number,
+            },
+            reps: {
+                type: Number,
+            },
+            sets: {
+                type: Number,
+            }
+        }]
+    })
+
+const Workout = mongoose.model("Workout", workoutSchema)
+    module.exports = Workout
